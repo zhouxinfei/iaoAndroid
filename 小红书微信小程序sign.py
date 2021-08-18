dcd64b97404ae21983d299d8889845e8fa6c3c01
iaodd <34562308+iaodd@users.noreply.github.com>
2021-08-18 18:11:27 +0800

    Create 小红书微信小程序sign.py

---
+++ 小红书微信小程序sign.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 小红书微信小程序sign 
+
+import hashlib
+
+
+def get_sign(url):
+    add_salt = url.split("www.xiaohongshu.com")[1] + "WSUDD"
+    Sign = "X" + hashlib.md5(add_salt.encode("utf-8")).hexdigest()
+    return Sign
